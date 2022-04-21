c244f92a24a70a111a5369657526f71055cc1fe3
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-04-21 16:39:06 +0700

    up

--- app/src/main/java/hcmute/danbaonguyen19110036/foody/Activity/LoginActivity.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/foody/Activity/LoginActivity.java
@@ -1,8 +1,11 @@
-package hcmute.danbaonguyen19110036.foody;
+package hcmute.danbaonguyen19110036.foody.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import hcmute.danbaonguyen19110036.foody.R;
+
 
 public class LoginActivity extends AppCompatActivity {
 
