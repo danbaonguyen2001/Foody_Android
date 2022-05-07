916f77f2c995847ef7e0c0d2f08350241927fa81
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-05-07 09:29:47 +0700

    update

---
+++ app/src/main/java/hcmute/danbaonguyen19110036/foody/Activity/OrdersActivity.java
@@ -0,0 +1,14 @@
+package hcmute.danbaonguyen19110036.foody.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import hcmute.danbaonguyen19110036.foody.R;
+public class OrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+    }
+}
