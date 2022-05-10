df77e65dcf7e22eed094b92d9ebf37bfc3b99cd6
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-05-10 21:55:29 +0700

    update

--- app/src/main/java/hcmute/danbaonguyen19110036/foody/Activity/CartActivity.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/foody/Activity/CartActivity.java
@@ -7,6 +7,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,16 +16,21 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import hcmute.danbaonguyen19110036.foody.Adapter.CartAdapter;
+import hcmute.danbaonguyen19110036.foody.Adapter.FoodAdapter;
 import hcmute.danbaonguyen19110036.foody.Database.DatabaseApplication;
+import hcmute.danbaonguyen19110036.foody.Database.Food;
 import hcmute.danbaonguyen19110036.foody.Database.FoodDao;
+import hcmute.danbaonguyen19110036.foody.Database.Shop;
 import hcmute.danbaonguyen19110036.foody.R;
 
 public class CartActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
     private Button btnCheckOut;
     private TextView txtTotal;
     private FoodDao foodDao;
+    ListView listView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +39,13 @@ public class CartActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_cart);
         ConnectDatabase();
-        recyclerView=findViewById(R.id.cart_list);
-        recyclerView.setHasFixedSize(true);
-        layoutManager= new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        listView = findViewById(R.id.listview_cart);
+        final List<Food> foodArrayList = foodDao.loadAll();
         btnCheckOut=findViewById(R.id.btnCheckOut);
         txtTotal=findViewById(R.id.tvTotalPrice);
+        CartAdapter cartAdapter = new CartAdapter(CartActivity.this,R.layout.cart_items,foodArrayList);
+        listView.setAdapter(cartAdapter);
+
     }
     public void ConnectDatabase(){
         foodDao = DatabaseApplication.Instance().createFoodDao();
